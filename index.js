7f18ff1c3039a6e75413d4171dd791d3cbc6eaa0
SHEHEERALIZX <nssheheerali@gmail.com>
2021-12-20 22:28:34 +0530

    Inital Commit

---
+++ index.js
@@ -0,0 +1,62 @@
+require('dotenv').config()
+
+var express = require('express');
+const mongoose = require('mongoose')
+var app = express();
+
+mongoose.connect(process.env.dbUrl, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+
+})
+
+const Shorturl = require('./models/urlShort')
+
+
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+
+
+
+app.get('/', async (req, res) => {
+
+   const shortUrls = await Shorturl.find()
+
+   res.render('index', { shortUrls: shortUrls })
+});
+
+
+app.post('/shortUrl', async (req, res) => {
+
+
+   await Shorturl.create({
+      full: req.body.fullUrl,
+      short: req.body.shortUrl
+
+   })
+
+   res.redirect('/')
+
+})
+
+
+app.get('/:shorturl',async (req,res)=>{
+    const url = await Shorturl.findOne({short: req.params.shorturl})
+    console.log(url);
+
+    if(url == null) return res.sendStatus(404)
+
+    url.clicks++;
+    url.save();
+    
+    res.redirect(url.full)
+    
+})
+app.listen(process.env.PORT || 8000);
+
+
+
+
+
+
